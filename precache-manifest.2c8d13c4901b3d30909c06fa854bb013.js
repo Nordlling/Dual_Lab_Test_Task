55e79da8b392f9c8cf22e01b9b21178d58b57a88
Toxa Nordling <toxa.podvorny@gmail.com>
2020-10-28 03:51:05 +0300

    Updates

---
+++ precache-manifest.2c8d13c4901b3d30909c06fa854bb013.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3543fbc51c274a829909b9fcf507961a",
+    "url": "/Dual_Lab_Test_Task/index.html"
+  },
+  {
+    "revision": "24f366717af18787bc18",
+    "url": "/Dual_Lab_Test_Task/static/css/main.28aec559.chunk.css"
+  },
+  {
+    "revision": "3206909302dc2823b756",
+    "url": "/Dual_Lab_Test_Task/static/js/2.289600de.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Dual_Lab_Test_Task/static/js/2.289600de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f366717af18787bc18",
+    "url": "/Dual_Lab_Test_Task/static/js/main.e331b8c6.chunk.js"
+  },
+  {
+    "revision": "47608306f7923e2ef6b7",
+    "url": "/Dual_Lab_Test_Task/static/js/runtime-main.469786e9.js"
+  },
+  {
+    "revision": "1233fdf19c04333c7f58af4eb8698452",
+    "url": "/Dual_Lab_Test_Task/static/media/Lato-Black.1233fdf1.ttf"
+  },
+  {
+    "revision": "eb9532033c2adf99b1314611b5e9cd0e",
+    "url": "/Dual_Lab_Test_Task/static/media/Lato-Bold.eb953203.ttf"
+  },
+  {
+    "revision": "90e1d3559ac52f7f0f77a86e1bfd632d",
+    "url": "/Dual_Lab_Test_Task/static/media/Lato-Light.90e1d355.ttf"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/Dual_Lab_Test_Task/static/media/Lato-Regular.3b9b9903.ttf"
+  }
+]);
